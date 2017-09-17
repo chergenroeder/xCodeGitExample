f59a1c580e2f70dc8b13aaf98f058ca4a9d20c7b
Chuck Hergenroeder <chergenroeder@gmail.com>
2017-09-17 12:22:57 -0400

    Initial Commit

---
+++ XcodeGitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeGitExample
+//
+//  Created by Chuck Hergenroeder on 9/17/17.
+//  Copyright © 2017 Chuck Hergenroeder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
